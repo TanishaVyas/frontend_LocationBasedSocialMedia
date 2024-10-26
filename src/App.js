96dab812180b717491f360b77255d109812ee8b5
satvikibudhia <122771370+satvikibudhia@users.noreply.github.com>
2024-10-27 01:47:59 +0530

    mongoose

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'; 
 
 function App() {
+  const [form, setForm] = useState({ username: '', password: '' }); // Initialize state
+
+  const handleForm = (e) => {
+    console.log(`${e.target.name}: ${e.target.value}`);
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+      const response = await fetch('http://localhost:8080/demo', {
+        method: 'POST',
+        body:JSON.stringify(form),
+        headers:{
+          'Content-Type':'application/json'
+        }
+      });
+      const data = await response.json();
+      console.log(data);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <p>{JSON.stringify(form)}</p>
+        <span>Username</span>
+        <input type="text" name="username" onChange={handleForm} value={form.username} />
+        <span>Password</span>
+        <input type="password" name="password" onChange={handleForm} value={form.password} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
