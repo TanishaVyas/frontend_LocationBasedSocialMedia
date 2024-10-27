c493134fdb91cccf1931e8c365fbec39cdf7c3a7
Tanisha Vyas <122625790+TanishaVyas@users.noreply.github.com>
2024-10-27 16:36:58 +0530

    connected with server

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import './App.css';
-import React, { useState } from 'react'; 
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [form, setForm] = useState({ username: '', password: '' }); // Initialize state
+  const [form, setForm] = useState({ username: "", password: "" }); // Initialize state
 
   const handleForm = (e) => {
     console.log(`${e.target.name}: ${e.target.value}`);
     setForm({
       ...form,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-      const response = await fetch('http://localhost:8080/demo', {
-        method: 'POST',
-        body:JSON.stringify(form),
-        headers:{
-          'Content-Type':'application/json'
-        }
-      });
-      const data = await response.json();
-      console.log(data);
-    
-  }
+
+    const response = await fetch("http://localhost:8080/api/users", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <p>{JSON.stringify(form)}</p>
         <span>Username</span>
-        <input type="text" name="username" onChange={handleForm} value={form.username} />
+        <input
+          type="text"
+          name="username"
+          onChange={handleForm}
+          value={form.username}
+        />
         <span>Password</span>
-        <input type="password" name="password" onChange={handleForm} value={form.password} />
+        <input
+          type="password"
+          name="password"
+          onChange={handleForm}
+          value={form.password}
+        />
         <input type="submit" />
       </form>
     </div>
