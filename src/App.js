b00e9f9d06b495b9648cd2362551e7a09c799315
Tanisha Vyas <122625790+TanishaVyas@users.noreply.github.com>
2024-10-28 13:32:44 +0530

    login working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import CompleteSignup from './components/CompleteSignup';
-import SignupWithGoogle from './components/SignupWithGoogle';
-import UserProfile from './components/UserProfile';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import CompleteSignup from "./components/CompleteSignup";
+import SignupWithGoogle from "./components/SignupWithGoogle";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-            <Route path="/" element={<SignupWithGoogle />} />
-            <Route path="/auth/complete-profile" element={<CompleteSignup />} />
-                <Route path="/complete-signup" element={<Login />} />
-                <Route path="/profile" element={<CompleteSignup />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignupWithGoogle />} />
+        <Route path="/auth/complete-profile" element={<CompleteSignup />} />
+        <Route path="/complete-signup" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
